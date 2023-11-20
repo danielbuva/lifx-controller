a210fb4e9bcdc6e2c2a9afff889f5705f38520d8
danielbuva <daniel.valdecantos@gmail.com>
2023-11-20 13:33:33 -0800

    chore: finish tailwind setup

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   plugins: [],
   theme: {},
 };
