5fb7af5570f8481cc44e9141dfacf250586d7a40
danielbuva <daniel.valdecantos@gmail.com>
2023-11-20 15:09:55 -0800

    chore: add cn util

--- client/src/lib/utils.ts
+++ client/src/lib/utils.ts
@@ -1,4 +1,11 @@
 import { QueryClient } from "@tanstack/react-query";
 
+import clsx, { type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+
 export const queryClient = new QueryClient();
 export const url = "http://localhost:3000/lights";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
