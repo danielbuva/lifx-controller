6407eec5caba5f4e4ac01c9cbd76713b0050df42
danielbuva <daniel.valdecantos@gmail.com>
2023-11-19 18:32:45 -0800

    refactor: export queryClient

--- client/src/components/QueryProvider.tsx
+++ client/src/components/QueryProvider.tsx
@@ -1,7 +1,6 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { QueryClientProvider } from "@tanstack/react-query";
 import { ReactNode } from "react";
-
-const queryClient = new QueryClient();
+import { queryClient } from "../global";
 
 export default function QueryProvider({
   children,
