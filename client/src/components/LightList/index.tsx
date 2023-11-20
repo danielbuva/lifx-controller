3a5e117f2cee46e62befbf53c03f8bef4fe9fbab
danielbuva <daniel.valdecantos@gmail.com>
2023-11-19 20:20:09 -0800

    chore: add tailwind

--- client/src/components/LightList/index.tsx
+++ client/src/components/LightList/index.tsx
@@ -5,19 +5,19 @@ export default function LightList() {
   const { data } = useLightsData();
   const toggleLight = useToggle();
 
+  if (!data) return null;
+
   return (
     <div>
-      {data &&
-        data.map((item) => (
-          <div key={item.id}>
-            <p>id: {item.id}</p>
-            <p>{item.label}</p>
-            <p>{item.power}</p>
-            <button onClick={() => toggleLight(item.id)}>
-              toggle power
-            </button>
-          </div>
-        ))}
+      {data.map((item) => (
+        <div key={item.id}>
+          <p>{item.label}</p>
+          <p>{item.power}</p>
+          <button onClick={() => toggleLight(item.id)}>
+            toggle power
+          </button>
+        </div>
+      ))}
     </div>
   );
 }
---
+++ client/tailwind.config.ts
@@ -0,0 +1,8 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
+  plugins: [],
+  theme: {},
+};
+export default config;
