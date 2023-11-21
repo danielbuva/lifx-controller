83169e182163f5118c8f059a1abd71e5bff427b0
danielbuva <daniel.valdecantos@gmail.com>
2023-11-20 23:40:21 -0800

    chore: remove unused file

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,6 @@
 import Layout from "./components/wrappers/Layout";
 import LightGroups from "./components/LightGroups";
 
-import "./App.css";
-
 export default function App() {
   return (
     <Layout>
