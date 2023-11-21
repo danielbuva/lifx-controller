83fb40b99cf08dedbcc55910dfc03b56b40e7814
danielbuva <daniel.valdecantos@gmail.com>
2023-11-20 17:15:02 -0800

    feat: add util to change hsbk to hsl

--- client/src/lib/utils.ts
+++ client/src/lib/utils.ts
@@ -9,3 +9,32 @@ export const url = "http://localhost:3000/lights";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function colorFromHsbk({
+  hue,
+  saturation,
+  kelvin,
+}: {
+  hue: number;
+  saturation: number;
+  kelvin: number;
+}) {
+  saturation *= 100;
+  if (saturation === 0) {
+    // when saturation is zero, adjust hue based on kelvin
+    const kelvinMin = 1500,
+      kelvinMax = 9000;
+    const kelvinRange = kelvinMax - kelvinMin;
+
+    // normalize kelvin value to [0, 1]
+    const normalizedKelvin = (kelvin - kelvinMin) / kelvinRange;
+
+    // map kelvin to a hue value from orange (30) to blue (240)
+    hue = 30 + normalizedKelvin * (240 - 30);
+
+    // set saturation to a default value for visibility
+    saturation = 80;
+  }
+
+  return `hsl(${hue}, ${saturation}%, 50%)`;
+}
