37df58e99328a6faa9f86084cd4d4159fba79b70
danielbuva <daniel.valdecantos@gmail.com>
2023-11-21 00:15:32 -0800

    fix: exit animation when entering expanded view

--- client/src/components/LightGroups/GroupCard/GroupCardLight.tsx
+++ client/src/components/LightGroups/GroupCard/GroupCardLight.tsx
@@ -11,9 +11,9 @@ import type { Light } from "../../../lib/types";
 import { itemVariant } from "./utils";
 
 const expansion = {
-  left: { x: -100 },
+  left: { x: -90 },
   center: { x: 0, y: 0, transition: { delay: 0.2 } },
-  right: { x: 100 },
+  right: { x: 90 },
 };
 
 export function GroupCardLight({ light }: { light: Light }) {
@@ -22,21 +22,22 @@ export function GroupCardLight({ light }: { light: Light }) {
   const { activelight, setActiveLight } = useActiveLight();
 
   return (
-    <AnimatePresence mode="wait">
-      <motion.div
-        // hack because idk how to animate layout when removing element
-        layout
-        style={{ height: activelight?.id === light.id ? 0 : "64px" }}
-      >
+    <motion.div
+      // hack border needs to be outside here or else exit animation doesn't play idk why :)
+      className={cn("border-2", {
+        "border-white": theme === "light",
+        "border-black": theme === "dark",
+        "border-gray-400": border,
+      })}
+      layout
+      // hack because idk how to animate layout when removing element : )
+      style={{ height: activelight?.id === light.id ? 0 : "64px" }}
+    >
+      <AnimatePresence mode="wait">
         {activelight?.id !== light.id && (
           <motion.div
             className={cn(
-              "p-4 flex w-80 justify-between border-2 cursor-pointer h-16",
-              {
-                "border-white": theme === "light",
-                "border-black": theme === "dark",
-                "border-gray-400": border,
-              }
+              "p-4 flex w-80 justify-between cursor-pointer h-16"
             )}
             layout
             onClick={() => {
@@ -73,7 +74,7 @@ export function GroupCardLight({ light }: { light: Light }) {
             </motion.div>
           </motion.div>
         )}
-      </motion.div>
-    </AnimatePresence>
+      </AnimatePresence>
+    </motion.div>
   );
 }
