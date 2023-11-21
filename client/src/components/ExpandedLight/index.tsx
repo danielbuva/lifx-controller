a274ae18a863870f384a66ea5e67526aede4f452
danielbuva <daniel.valdecantos@gmail.com>
2023-11-21 12:59:00 -0800

    style(expanded): add enter and exit animations

--- client/src/components/ExpandedLight/index.tsx
+++ client/src/components/ExpandedLight/index.tsx
@@ -5,6 +5,12 @@ import { useToggle } from "../../hooks/post";
 import Switch from "../styled/Switch";
 import { hsbkToHsl } from "../../lib/utils";
 
+const expanded = {
+  left: { x: -90 },
+  origin: { x: 0, y: 0, transition: { delay: 0.6 } },
+  right: { x: 90 },
+};
+
 export default function ExpandedLight() {
   const { activelight } = useActiveLight();
   const ref = useClickOutsideExpandedLight();
@@ -13,19 +19,36 @@ export default function ExpandedLight() {
     <AnimatePresence mode="wait">
       {activelight && (
         <motion.div
-          className="absolute w-3/5 h-2/4 flex border-2 bg-theme p-4"
+          className="absolute w-3/5 h-2/4 flex border-2 bg-theme p-6 overflow-hidden"
           onClick={(e) => e.stopPropagation()}
           ref={ref}
           initial={{ opacity: 0, scale: 0, y: -100 }}
           animate={{ opacity: 1, scale: 1, y: 0 }}
+          exit={{ opacity: 0, scale: 0, transition: { delay: 0.5 } }}
           transition={{ delay: 0.2 }}
         >
-          <motion.h2>{activelight.label}</motion.h2>
-          <Switch
-            color={hsbkToHsl(activelight.color)}
-            power={activelight.power}
-            toggle={() => toggle(activelight.id)}
-          />
+          <div className="w-full flex justify-between">
+            <motion.h2
+              animate="origin"
+              initial="left"
+              exit="left"
+              variants={expanded}
+            >
+              {activelight.label}
+            </motion.h2>
+            <motion.div
+              animate="origin"
+              initial="right"
+              exit="right"
+              variants={expanded}
+            >
+              <Switch
+                color={hsbkToHsl(activelight.color)}
+                power={activelight.power}
+                toggle={() => toggle(activelight.id)}
+              />
+            </motion.div>
+          </div>
         </motion.div>
       )}
     </AnimatePresence>
