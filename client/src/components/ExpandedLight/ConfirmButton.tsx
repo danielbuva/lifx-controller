3226a5a100dc19121c0bb9bef72bb40f3d9caad1
danielbuva <daniel.valdecantos@gmail.com>
2023-11-22 17:21:19 -0800

    feat: add cooldown to confirm button

--- client/src/components/ExpandedLight/ConfirmButton.tsx
+++ client/src/components/ExpandedLight/ConfirmButton.tsx
@@ -2,18 +2,29 @@ import { useLightState } from "@/hooks/put";
 import useActiveLight from "@/hooks/useActiveLight";
 import useSlideData from "@/hooks/useSliderData";
 import type { LightConfigState } from "@/lib/types";
+import { cn } from "@/lib/utils";
+import { useState } from "react";
 
 export default function ConfirmButton() {
+  const [isOnCooldown, setIsOnCooldown] = useState(false);
   const setLightState = useLightState();
   const { lightConfig } = useSlideData();
   const { activeLight } = useActiveLight();
   if (!activeLight) return null;
   const handleClick = () => {
+    setIsOnCooldown(true);
     setLightState({ id: activeLight.id, color: createBody(lightConfig) });
+    setTimeout(() => {
+      setIsOnCooldown(false);
+    }, 1000);
   };
   return (
     <button
-      className="w-44 h-9 rounded-md justify-center items-center flex"
+      className={cn(
+        "w-44 h-9 rounded-md justify-center items-center flex",
+        { "cursor-wait": isOnCooldown }
+      )}
+      disabled={isOnCooldown}
       onClick={handleClick}
     >
       confirm
