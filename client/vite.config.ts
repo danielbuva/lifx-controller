1d75606cab5bd1603bde61f80363e80fc179708b
danielbuva <daniel.valdecantos@gmail.com>
2023-11-24 19:45:36 -0800

    chore: extend vite and ts config for server paths

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,4 +1,5 @@
 import react from "@vitejs/plugin-react";
+import { resolve } from "path"; // Import the resolve function
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
@@ -6,7 +7,8 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": "/src",
+      "@": resolve(__dirname, "./src"), // Adjusted path for 'src'
+      "@server": resolve(__dirname, "../server/src"), // Adjusted path for 'server/src'
     },
   },
 });
