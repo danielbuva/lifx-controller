659e2b4a291329d8042517baace52c730568c3c0
danielbuva <daniel.valdecantos@gmail.com>
2023-11-25 00:33:42 -0800

    feat: add add preset function

--- client/src/lib/elysia.ts
+++ client/src/lib/elysia.ts
@@ -1,5 +1,6 @@
 import { edenTreaty } from "@elysiajs/eden";
 import type { App } from "@server/index";
+import type { Preset } from "@server/types";
 
 export const client = edenTreaty<App>("http://localhost:3000");
 
@@ -16,3 +17,9 @@ export async function setLightState({
 }) {
   await client.lights["id:" + id]?.state.put({ color });
 }
+
+export async function addPreset(preset: Preset) {
+  await client.lights.presets.add.post(preset);
+}
+
+export const { data: lights } = await client.lights.get();
