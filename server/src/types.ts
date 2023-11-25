40a2f5c54d445d4cd8634698bfe8249b1d54dda0
danielbuva <daniel.valdecantos@gmail.com>
2023-11-25 00:33:11 -0800

    chore: use type

--- server/src/types.ts
+++ server/src/types.ts
@@ -1,24 +1,9 @@
+import type { Preset as dbPreset } from "@prisma/client";
+
 export enum Power {
   ON = "on",
   OFF = "off",
 }
-
-export type LightsResponse = Promise<
-  {
-    brightness: number;
-    color: { hue: number; saturation: number; kelvin: number };
-    connected: boolean;
-    effect: string;
-    group: { id: string; name: string };
-    label: string;
-    last_seen: string;
-    location: { id: string; name: string };
-    id: string;
-    power: Power;
-    seconds_since_seen: number;
-    uuid: string;
-  }[]
->;
 
 export type Light = {
   brightness: number;
@@ -35,9 +20,12 @@ export type Light = {
   uuid: string;
 };
 
+export type LightsResponse = Promise<Light[]>;
 export interface GroupInfo {
   groupName: string;
   groupId: string;
   lights: Light[];
   power: Power;
 }
+
+export type Preset = dbPreset;
