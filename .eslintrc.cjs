5275df2e7224b30b19f116d275b232f55e158ead
danielbuva <daniel.valdecantos@gmail.com>
2023-11-25 00:30:47 -0800

    chore: move eslint rc to root

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,3 +1,4 @@
+/** @type {import("eslint").Linter.Config} */
 module.exports = {
   root: true,
   env: { browser: true, es2021: true },
@@ -7,14 +8,52 @@ module.exports = {
     "plugin:react-hooks/recommended",
     "plugin:import/typescript",
   ],
+  overrides: [
+    {
+      files: ["client/**/*.{js,jsx,ts,tsx}"],
+      env: {
+        browser: true,
+      },
+      // ... other client-specific settings
+    },
+    // ... other overrides
+  ],
   ignorePatterns: ["dist", ".eslintrc.cjs"],
   parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: "latest",
+    project: [
+      "./tsconfig.json",
+      "./client/tsconfig.json",
+      "./server/tsconfig.json",
+      "./tsconfig.eslint.json",
+    ],
+    sourceType: "module",
+    tsconfigRootDir: __dirname,
+  },
   plugins: [
     "no-relative-import-paths",
     "react-refresh",
     "eslint-plugin-import-helpers",
   ],
   rules: {
+    "@typescript-eslint/consistent-type-imports": "error",
+    "@typescript-eslint/no-empty-function": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-floating-promises": "error",
+    "@typescript-eslint/no-non-null-assertion": "off",
+    "@typescript-eslint/no-unused-vars": [
+      "warn",
+      {
+        argsIgnorePattern: "^_",
+        caughtErrorsIgnorePattern: "^_",
+        destructuredArrayIgnorePattern: "^_",
+        varsIgnorePattern: "^_",
+      },
+    ],
     "react-refresh/only-export-components": [
       "warn",
       { allowConstantExport: true },
