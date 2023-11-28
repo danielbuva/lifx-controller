71cccc2a377926f0a1624bd68b4e21d8c605aa64
danielbuva <daniel.valdecantos@gmail.com>
2023-11-28 14:28:00 -0800

    refactor(preset): dont destructure props

--- client/src/components/PresetsCard/Preset.tsx
+++ client/src/components/PresetsCard/Preset.tsx
@@ -1,67 +1,58 @@
 import { cn, kelvinToHsl } from "@/lib/utils";
 import type { Preset } from "@server/types";
 
-export default function Preset({
-  id,
-  label,
-  hue,
-  saturation,
-  lightness,
-  kelvin,
-  lightId,
-  brightness,
-  handleClick,
-  handleDelete,
-  isOnCooldown,
-  groupId,
-}: Preset & {
-  handleClick: (
-    kelvin: number | null,
-    lightId: string,
-    brightness: number,
-    lightness: number | null,
-    saturation: number | null,
-    hue: number | null,
-    groupId: string
-  ) => Promise<void>;
-  handleDelete: (id: number) => Promise<void>;
-  isOnCooldown: boolean;
-}) {
+export default function Preset(
+  props: Preset & {
+    handleClick: (
+      kelvin: number | null,
+      lightId: string,
+      brightness: number,
+      lightness: number | null,
+      saturation: number | null,
+      hue: number | null,
+      groupId: string
+    ) => Promise<void>;
+    handleDelete: (id: number) => Promise<void>;
+    isOnCooldown: boolean;
+  }
+) {
   let backgroundColor: string;
-  if (kelvin) {
-    const { hue, saturation, lightness } = kelvinToHsl(kelvin);
+  if (props.kelvin) {
+    const { hue, saturation, lightness } = kelvinToHsl(props.kelvin);
     backgroundColor = `hsl(${hue}, ${saturation * 100}%, ${lightness}%)`;
   } else {
-    backgroundColor = `hsl(${hue}, ${saturation! * 100}%, ${lightness}%)`;
+    backgroundColor = `hsl(${props.hue}, ${props.saturation! * 100}%, ${
+      props.lightness
+    }%)`;
   }
 
   return (
     <div
       className={cn(
         "w-24 h-16 rounded-md text-white m-2 p-1 cursor-pointer relative",
-        { "cursor-wait": isOnCooldown }
+        { "cursor-wait": props.isOnCooldown }
       )}
       style={{
         backgroundColor,
       }}
       onClick={async () =>
-        handleClick(
-          kelvin,
-          lightId,
-          brightness,
-          lightness,
-          saturation,
-          hue,
-          groupId
+        props.handleClick(
+          props.kelvin,
+          props.lightId,
+          props.brightness,
+          props.lightness,
+          props.saturation,
+          props.hue,
+          props.groupId
         )
       }
     >
-      {label}
+      {props.label}
       <div
         className="absolute top-[-8px] right-[-7px] text-black bg-theme flex flex-row justify-center items-center rounded-full w-4 h-4 border-[1px] border-inverseTheme m-0 p-0 cursor-pointer opacity-0 hover:opacity-100"
         onClick={async (e) => {
           e.stopPropagation();
-          await handleDelete(id);
+          await props.handleDelete(props.id);
         }}
       >
         x
