49a99053dc9951ada9711083acaeaf6c8297800c
danielbuva <daniel.valdecantos@gmail.com>
2023-11-29 13:08:12 -0800

    feat: add header to layout

--- client/src/components/PresetsCard/index.tsx
+++ client/src/components/PresetsCard/index.tsx
@@ -2,7 +2,7 @@ import Presets from "./Presets";
 
 export default function PresetsCard() {
   return (
-    <div className="w-full shadow-theme rounded-md overflow-hidden bg-theme">
+    <div className="shadow-theme rounded-md overflow-hidden bg-theme">
       <div className="flex flex-row justify-between bg-slate-600 p-4 rounded-tl-md rounded-tr-md">
         <h2 className="text-xl font-semibold text-white">presets</h2>
       </div>
--- client/src/components/wrappers/Layout.tsx
+++ client/src/components/wrappers/Layout.tsx
@@ -1,3 +1,4 @@
+import GithubSVG from "@/components/svgs/GithubSVG";
 import useLifxState from "@/hooks/useLifxState";
 import type { ReactNode } from "react";
 
@@ -5,10 +6,31 @@ export default function Layout({ children }: { children: ReactNode }) {
   const { setActiveLightIndices } = useLifxState();
   return (
     <div
-      className="min-h-screen w-screen flex-row flex justify-center items-center"
+      className="min-h-screen w-screen flex-col flex justify-start"
       onClick={() => setActiveLightIndices(null)}
     >
-      <div className="flex flex-col gap-4">{children}</div>
+      <Header />
+      <div className="flex flex-col gap-4 items-center">{children}</div>
+    </div>
+  );
+}
+
+function Header() {
+  return (
+    <div className="p-6 w-full mb-14 flex justify-between ">
+      <p className="relative w-full text-xl">
+        lifx remote
+        <span className="text-sm absolute left-[100px] top-[-12px]">
+          &nbsp;&nbsp;&nbsp;{"__A__"} <br />
+          {`( ~ o'_'o)~---<>  ⁂` /**  pew pew pew */}
+        </span>
+      </p>
+      <a
+        href="https://github.com/danielbuva/lifx-controller"
+        target="_blank"
+      >
+        <GithubSVG className="cursor-pointer" />
+      </a>
     </div>
   );
 }
