3c89972682698b09545e6994039025a88e55fcd3
danielbuva <daniel.valdecantos@gmail.com>
2023-11-29 17:59:04 -0800

    chore: add comment about strict mode

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,11 +1,10 @@
-// import React from "react";
 import ReactDOM from "react-dom/client";
 
 import App from "./App.tsx";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  // <React.StrictMode>
-  <App />
-  // </React.StrictMode>
-);
+// strict mode off because initial app state comes from lifx.
+// when dispatching power off/on the second initialization of state
+// is different from the first so switch states visually can't toggle.
+
+ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
